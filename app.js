48d8393940beb061e83e971d70896847d6ab0639
Pierre Le Fevre <hello@pierrelf.com>
2022-12-07 18:15:07 +0100

    add /v1/ path

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-import env from './environment.js'
+import env from "./environment.js";
 
-import express from 'express';
-import session from 'express-session';
-import fileupload from 'express-fileupload';
-import bodyParser from 'body-parser';
+import express from "express";
+import session from "express-session";
+import fileupload from "express-fileupload";
+import bodyParser from "body-parser";
 
-import status from './routes/status.js';
-import capacities from './routes/capacities.js';
-import news from './routes/news.js';
-import stats from './routes/stats.js'
-import { memoryStore } from './common.js'
-import { keycloak } from './security.js';
-import cors from 'cors'
+import status from "./routes/status.js";
+import capacities from "./routes/capacities.js";
+import news from "./routes/news.js";
+import stats from "./routes/stats.js";
+import { memoryStore } from "./common.js";
+import { keycloak } from "./security.js";
+import cors from "cors";
 
 const app = express();
 
-app.use(cors())
-app.use(session({
+app.use(cors());
+app.use(
+  session({
     secret: env.sessionSecret,
     resave: false,
     saveUninitialized: true,
-    store: memoryStore
-}))
+    store: memoryStore,
+  })
+);
 app.use(fileupload());
-app.use(keycloak.middleware({ logout: '/'}))
+app.use(keycloak.middleware({ logout: "/" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use('/', status, capacities, news, stats);
+app.use("/v1/", status, capacities, news, stats);
 
 const port = env.LANDING_PORT || 8080;
 
 app.listen(port);
-console.log('server started on port: ' + port);
-
+console.log("server started on port: " + port);
